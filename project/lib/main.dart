ac048f5ecfe54ae3261f2d6f6f80b8de725ffeca
DeenankSharma <sharmadeenank@gmail.com>
2024-07-18 16:19:15 +0530

    uploads files

---
+++ project/lib/main.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple
+      ),
+      home: Container_Sized(),
+    );
+  }
+}
